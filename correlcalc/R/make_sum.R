116faa8bf8bd201ce5f3d70fbf7eb56685b087a4
Cosima Meyer <cosima.meyer@gmail.com>
2021-11-11 22:16:17 +0100

    Add files via upload

---
+++ correlcalc/R/make_sum.R
@@ -0,0 +1,11 @@
+#' @title Simple function that calculates the sum
+#' @description This function takes two argument (`a` and `b`) and calculates the sum of both.
+#' @param a summand (numeric)
+#' @param b summand (numeric)
+#' @examples
+#' \dontrun{make_sum(2, 3)}
+
+make_sum <- function(a, b) {
+  c <- a + b
+  return(c)
+}
